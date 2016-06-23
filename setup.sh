67798c025fcd16f1092b3620120f3c1ff37f2a46
Junichi Kobayashi <junichi.kobayashi@esm.co.jp>
2016-06-23 17:38:18 +0900

    Add gemrc

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-DOT_FILES=(.zshrc .zprofile .zshenv .gitconfig .vimrc .tmux.conf .pryrc)
+DOT_FILES=(.zshrc .zprofile .zshenv .gitconfig .vimrc .tmux.conf .pryrc .gemrc)
 
 for file in ${DOT_FILES[@]}
 do
