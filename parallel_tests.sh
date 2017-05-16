d1a18395fad8b085b40b26d9bb38a38662be02b2
Junichi Kobayashi <nnujabok@gmail.com>
2017-05-16 16:48:17 +0900

    Add parallel_test settings

--- parallel_tests.sh
+++ parallel_tests.sh
@@ -1,4 +1,7 @@
 #!/bin/zsh
 
+alias parallel_db_environment_set="bundle exec parallel_test --exec 'rails db:environment:set RAILS_ENV=test' -n 4"
 alias parallel_db_schema_load="bundle exec parallel_test --exec 'rake db:schema:load RAILS_ENV=test' -n 4"
 alias parallel_spec="RAILS_ENV=test bundle exec parallel_test spec --type rspec -n 4 --suffix '_spec.rb|.feature'"
+
+alias all_spec='parallel_db_environment_set && parallel_db_schema_load && parallel_spec'
