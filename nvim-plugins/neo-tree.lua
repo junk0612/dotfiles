2f73291698c7a8917c9d33b57a5d3a67dc6983f2
Junichi Kobayashi <nnujabok@gmail.com>
2024-10-06 23:55:50 +0900

    Show all hidden files in NeoTree

--- nvim-plugins/neo-tree.lua
+++ nvim-plugins/neo-tree.lua
@@ -7,6 +7,13 @@ return {
     "MunifTanjim/nui.nvim",
   },
   config = function()
+    require('neo-tree').setup {
+      filesystem = {
+	filtered_items = {
+	  visible = true, -- This is what you want: If you set this to `true`, all "hide" just mean "dimmed out"
+	},
+      },
+    }
     vim.keymap.set("n", "<Space>ff", ":Neotree toggle=true<CR>")
   end
 }
