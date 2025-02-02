61400c36ed77bea5ec5c2259ccfa8b85867a80ff
Junichi Kobayashi <nnujabok@gmail.com>
2025-02-02 16:29:05 +0900

    Place .gitignore and .gitconfig to $HOME/.config/git

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
 #!/bin/zsh
-DOT_FILES=(.zshrc .zprofile .zshenv .gitconfig .vimrc .tmux.conf .pryrc .gemrc .railsrc .default-gems .rubocop.yml .gitignore_global)
+DOT_FILES=(.zshrc .zprofile .zshenv .vimrc .tmux.conf .pryrc .gemrc .railsrc .default-gems .rubocop.yml)
 
 for file in ${DOT_FILES[@]}
 do
@@ -9,3 +9,7 @@ done
 mkdir -p $HOME/.config/nvim/lua
 ln -s $HOME/dotfiles/init.lua $HOME/.config/nvim/init.lua
 ln -s $HOME/dotfiles/nvim-plugins $HOME/.config/nvim/lua/plugins
+
+mkdir -p $HOME/.config/git
+ln -s $HOME/dotfiles/.gitconfig $HOME/.config/git/config
+ln -s $HOME/dotfiles/.gitignore $HOME/.config/git/ignore
