8ad78c0c31dfd35109d1c19769e689d44ef78855
Junichi Kobayashi <nnujabok@gmail.com>
2025-02-05 23:46:11 +0900

    Update nvim-treesitter config to enable treesitter

--- nvim/lua/plugins/nvim-treesitter.lua
+++ nvim/lua/plugins/nvim-treesitter.lua
@@ -1,10 +1,14 @@
 return {
-  "nvim-treesitter/nvim-treesitter",
-  build = function()
-    require("nvim-treesitter.install").update({ with_sync = true })()
-  end,
-  opts = {
-    highlight = { enable = true },
-    indent = { enable = true },
-  },
+  {
+    "nvim-treesitter/nvim-treesitter",
+    build = function()
+      require("nvim-treesitter.install").update({ with_sync = true })()
+    end,
+    config = function()
+      require("nvim-treesitter.configs").setup({
+        highlight = { enable = true },
+        indent = { enable = true },
+      })
+    end,
+  }
 }
