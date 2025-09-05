b01b86a8154c18c43c364b0d3f6e94eaad33a8e6
Junichi Kobayashi <nnujabok@gmail.com>
2025-09-05 18:10:17 +0900

    Update Neovim highlight settings

--- nvim/lua/config/highlight.lua
+++ nvim/lua/config/highlight.lua
@@ -1,4 +1,6 @@
+vim.cmd.highlight("clear")
+
 vim.api.nvim_set_hl(0, "Normal", { bg='None' })
-vim.api.nvim_set_hl(0, "NormalFloat", { bg='DarkCyan' })
+vim.api.nvim_set_hl(0, "CursorLine", { bg='None' })
 vim.api.nvim_set_hl(0, "LineNr", { fg='White' })
-vim.api.nvim_set_hl(0, "CursorLineNr", { fg='Yellow' })
+vim.api.nvim_set_hl(0, "CursorLineNr", { fg='Yellow', bold = true })
