f03ef196d6c89b7dde52e032458f58efc5b18ddb
Junichi Kobayashi <nnujabok@gmail.com>
2025-09-19 16:02:44 +0900

    Add Codex config setup

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
 #!/bin/zsh
-DOT_FILES=(.zshrc .zprofile .zshenv .tmux.conf .gemrc .railsrc .default-gems .rubocop.yml)
+DOT_FILES=(.zshrc .zprofile .zshenv .tmux.conf .gemrc .railsrc .default-gems .rubocop.yml .codex)
 
 for file in ${DOT_FILES[@]}
 do
@@ -9,3 +9,6 @@ done
 mkdir -p $HOME/.config
 ln -s -t $HOME/.config/ $HOME/dotfiles/nvim
 ln -s -t $HOME/.config/ $HOME/dotfiles/git
+
+mkdir -p $HOME/.codex
+ln -s $HOME/dotfiles/codex/config.toml $HOME/.codex/config.toml
