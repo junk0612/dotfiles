0d8e93abb5de3eb9ebdc0f35bd1091ffa41fee86
Junichi Kobayashi <nnujabok@gmail.com>
2026-07-14 23:45:51 +0900

    Remove empty .zprofile

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
 #!/bin/zsh
-DOT_FILES=(.zshrc .zprofile .zshenv .tmux.conf .gemrc .railsrc .default-gems .rubocop.yml .tool-versions)
+DOT_FILES=(.zshrc .zshenv .tmux.conf .gemrc .railsrc .default-gems .rubocop.yml .tool-versions)
 
 for file in ${DOT_FILES[@]}
 do
